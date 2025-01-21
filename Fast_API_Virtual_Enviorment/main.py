04f3f84c4ad4d9f1115480cecd1a4d1b9ee66634
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-01-21 15:57:50 +0500

    Uvivorn standard

--- Fast_API_Virtual_Enviorment/main.py
+++ Fast_API_Virtual_Enviorment/main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BANDS = [
+    {'Id' : 1},
+]
+
 @app.get("/")
 async def getData() -> dict[str,str]:
     return {"message":"FAST API"}
+
+@app.get("/about")
+async def about() -> str:
+    return "A Great Company"
