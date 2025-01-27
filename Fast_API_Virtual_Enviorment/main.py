a5ef1e2c793942455fd46653919053b5317e3d14
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-01-27 15:13:55 +0500

    Request Body and POST requests | Pydantic pre-validators

--- Fast_API_Virtual_Enviorment/main.py
+++ Fast_API_Virtual_Enviorment/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,HTTPException
-from schemas import Bands,GenreURL
+from schemas import Bands,GenreURL,BandCreate,BandWithId
 app = FastAPI()
 
 
@@ -22,8 +22,8 @@ BANDS = [
 
 
 @app.get("/bands")
-async def getData(has_album : bool = False,genre:GenreURL|None = None) -> list[Bands]:
-    band_List =  [Bands(**b) for b in BANDS]
+async def getData(has_album : bool = False,genre:GenreURL|None = None) -> list[BandWithId]:
+    band_List =  [BandWithId(**b) for b in BANDS]
     if has_album:
         band_List = [band for band in band_List if 'album' in band]
         
@@ -35,12 +35,19 @@ async def getData(has_album : bool = False,genre:GenreURL|None = None) -> list[B
 
 @app.get("/band/{band_id}")
 async def getSingleBand(band_id : int):
-    band = next((Bands(**b) for b in BANDS if b['id'] == band_id),None)
+    band = next((BandWithId(**b) for b in BANDS if b['id'] == band_id),None)
     if band is None:
         raise HTTPException(status_code=404,detail="Not Found")
     return band
 
 @app.get("/band/genre/{band_genre}")
 async def getSingleBand(band_genre : GenreURL):
-    return [Bands(**b) for b in BANDS if b['genre'].lower() == band_genre.value]
+    return [BandWithId(**b) for b in BANDS if b['genre'].lower() == band_genre.value]
   
+
+@app.post("/band_create/")
+async def createBand(band_data : BandCreate)->BandWithId:
+    id = BANDS[-1]['id'] + 1
+    band = BandWithId(id=id, **band_data.model_dump()).model_dump()
+    BANDS.append(band)
+    return band
--- Fast_API_Virtual_Enviorment/schemas.py
+++ Fast_API_Virtual_Enviorment/schemas.py
@@ -1,19 +1,30 @@
-from enum  import Enum
+from enum import Enum
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, validator
 
-class GenreURL(Enum):
+class GenreURL(str, Enum):
     ROCK = 'rock'
     ELECTRONIC = 'electronics'
     SHOEGAZA = 'shoegaza'
     HIPHOP = 'hip-hop'
 
 class Album(BaseModel):
-    title : str
-    releasedDate : date
+    title: str
+    releasedDate: date
 
 class Bands(BaseModel):
-    id: int
     name: str
-    genre: str
-    album : list[Album] = []
+    genre: GenreURL
+    album: list[Album] = []
+
+class BandCreate(Bands):
+    @validator('genre', pre=True)
+    def validate_genre(cls, v):
+        if isinstance(v, str):
+            v = v.lower()
+        if v not in GenreURL._value2member_map_:
+            raise ValueError(f"Invalid genre: {v}")
+        return GenreURL(v)
+
+class BandWithId(Bands):
+    id: int
