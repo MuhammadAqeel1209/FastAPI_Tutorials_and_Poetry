657d099ea337176c6303edaf59b40d1ff222c1d0
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-02-14 20:46:05 +0500

    Query Parameter

---
+++ Fast_API_Tutorial_Complete/Query_Parameter/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "hello world"}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_item(
+    item_id: str, sample_query_param: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+        return item
