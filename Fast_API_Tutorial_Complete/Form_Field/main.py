917aa654afe7511c6d842756197ae2eea975a0f1
Muhammad Aqeel <104295133+Aqeel785@users.noreply.github.com>
2025-02-15 18:03:38 +0500

    files requestUploadFilename

--- Fast_API_Tutorial_Complete/Form_Field/main.py
+++ Fast_API_Tutorial_Complete/Form_Field/main.py
@@ -1,5 +1,4 @@
-from fastapi import Body, FastAPI, Form
-
+from fastapi import Body, FastAPI, Form,File,UploadFile
 
 app = FastAPI()
 
@@ -8,3 +7,15 @@ app = FastAPI()
 async def login(username: str = Form(...), password: str = Body(...)):
     print("password", password)
     return {"username": username}
+@app.post("/files/")
+async def create_file(
+    files: list[bytes] = File(..., description="A file read as bytes")
+):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(
+    files: list[UploadFile] = File(..., description="A file read as UploadFile")
+):
+    return {"filename": [file.filename for file in files]}
